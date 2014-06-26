f6510ffa9ad5c34db121b96c9a7647bdb205c0a2
Anderson <anderson@anderson-desk.(none)>
2014-06-26 11:03:18 -0300

    #

--- chapter_6/searching.py
+++ chapter_6/searching.py
@@ -1,38 +1,47 @@
-# def sequentialSearch(alist, item):
-#     pos = 0
-#     found = False
+def sequentialSearch(alist, item):
+    """
+    Percorre a lista, item apos item,
+    ate achar o item procurado.
+    """
+    pos = 0
+    found = False
 
-#     while pos < len(alist) and not found:
-#         if alist[pos] == item:
-#             found = True
-#         else:
-#             pos = pos+1
+    while pos < len(alist) and not found:
+        if alist[pos] == item:
+            found = True
+        else:
+            pos = pos+1
 
-#     return found
+    return found
 
-# testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-# print(sequentialSearch(testlist, 3))
-# print(sequentialSearch(testlist, 13))
+testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+print(sequentialSearch(testlist, 3))
+print(sequentialSearch(testlist, 13))
 
 
-# def orderedSequentialSearch(alist, item):
-#     pos = 0
-#     found = False
-#     stop = False
-#     while pos < len(alist) and not found and not stop:
-#         if alist[pos] == item:
-#             found = True
-#         else:
-#             if alist[pos] > item:
-#                 stop = True
-#             else:
-#                 pos = pos+1
+def orderedSequentialSearch(alist, item):
+    """
+    Percorre a lista, item apos item, verificando o item 
+    item da lista e maior que o item procurado. Se for a
+    busca encerra.
+    """
+    pos = 0
+    found = False
+    stop = False
+    while pos < len(alist) and not found and not stop:
+        if alist[pos] == item:
+            found = True
+        else:
+            if alist[pos] > item:
+                stop = True
+            else:
+                pos = pos+1
 
-#     return found
+    return found
 
-# testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-# print(orderedSequentialSearch(testlist, 3))
-# print(orderedSequentialSearch(testlist, 13))
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(orderedSequentialSearch(testlist, 3))
+print(orderedSequentialSearch(testlist, 13))
 
 
 # def binarySearch(alist, item):
@@ -59,11 +68,11 @@
 
 
 
-def recursive_sequential_search(lista, a):
-    if not lista:
-        return False
-    if a == lista[0]:
-        return True
-    return recursive_sequential_search(lista[1:], a)
+# def recursive_sequential_search(lista, a):
+#     if not lista:
+#         return False
+#     if a == lista[0]:
+#         return True
+#     return recursive_sequential_search(lista[1:], a)
 
-print recursive_sequential_search([1,2,3], 4)
+# print recursive_sequential_search([1,2,3], 4)
