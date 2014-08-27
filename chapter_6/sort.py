e8b87dadabbf09699e689a31370573da63513539
Anderson Resende <andersonresende@mbp-de-anderson.home>
2014-08-27 19:27:05 -0300

    study selection sort

--- chapter_6/sort.py
+++ chapter_6/sort.py
@@ -1,10 +1,39 @@
-def bb(lista):
-	quant = len(lista)
-	for q in range(quant,0,-1):
-		for i in range(0,q-1):
-			if lista[i] > lista[i+1]:
-				lista[i], lista[i+1] = lista[i+1], lista[i]
-		print lista
-	return lista
+def bubble_sort(lst):
+    """Bubble sort standard, loop many times for each item, it's not good"""
+    qt = len(lst)
+    for q in range(qt, 0,  -1):
+        for i in range(0,q-1):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+    return lst
 
-print bb([7,5,8,3,4,2,9])
+#call function
+print "Bubble sort"
+print bubble_sort([7, 5, 8, 3, 4, 2, 9])
+
+
+def short_bubble_sort(lst):
+    """
+    Function that sort a list, stop when not exchanges.
+    It's more effective than normal bubble sort.
+    """
+    exchange = True
+    rounds = len(lst) - 1
+    while exchange and rounds:
+        exchange = False
+        for index in range(0, rounds):
+            if lst[index] > lst[index+1]:
+                lst[index], lst[index+1] = lst[index+1], lst[index]
+                exchange = True
+        rounds -= 1
+    return lst
+
+
+#create random list
+import random
+lst = range(10)
+random.shuffle(lst)
+#call function
+print "Short bubble sort:"
+print "Unordered lst: %s" % lst
+print "Ordered lst: %s" % short_bubble_sort(lst)
---
+++ myscripts/temp_selection_sort.py
@@ -0,0 +1,6 @@
+#Por que nao setamos qualquer valor da lista como o maior em uma selection sort?
+#Qual a diferenca entre a selection sort e a bubble sort? bubble faz mais trocas, no entanto, as
+# duas percorrem os mesmos loops. Buble comprar item com o proximo item, buble compara item com o maior que ja passou.
+
+
+
