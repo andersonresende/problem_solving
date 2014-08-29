88c6b653f7f4570ede6ff87b5680ff7da39ded79
resende <anderson.resende@gmail.com>
2014-08-29 18:31:15 -0300

    organizando e dividindo codigo

--- chapter_6/selection_sort.py
+++ chapter_6/selection_sort.py
@@ -1,9 +1,7 @@
-#Por que nao setamos qualquer valor da lista como o maior em uma selection sort?
-#Qual a diferenca entre a selection sort e a bubble sort? bubble faz mais trocas, no entanto, as
-# duas percorrem os mesmos loops. Buble comprar item com o proximo item, select compara item com o maior que ja passou.
-
-
 def selection_sort(lst):
+    """
+    Function select sort that order a list.
+    """
     for loop in range(len(lst)-1, 0, -1):
         max_location = 0
         for l in range(1, loop+1):
@@ -18,6 +16,10 @@ print selection_sort([1, 2, 5, 3, 7, 4])
 
 
 def while_selection_sort(lst):
+    """
+    Function select sort that order a list,
+    Implements whith while loops instead for.
+    """
     cont = len(lst) - 1
     while cont:
         c = 1
@@ -30,16 +32,8 @@ def while_selection_sort(lst):
         cont -= 1
     return lst
 
+
 print while_selection_sort([1, 2, 5, 3, 7, 4])
-
-
-for i in range(1, 6):
-    print i
-
-c = 1
-while c < 6:
-    print c
-    c += 1
 
 
 
